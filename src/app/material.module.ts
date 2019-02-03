b680519eca5f683c2dba18b7bf61143e64e21c9e
vijay vittal <vijay.vittal@votercircle.com>
2019-02-03 12:34:03 +0530

    option to enter event added

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,17 +2,23 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/';
 
 @NgModule({
     declarations: [],
     imports: [
         CommonModule,
         MatButtonModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatFormFieldModule,
+        MatInputModule
     ],
     exports: [
         MatButtonModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatFormFieldModule,
+        MatInputModule
     ]
 })
 export class MaterialModule { }
